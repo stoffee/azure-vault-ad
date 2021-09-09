42b9c037554392302b80a5461e4b9e4ecacb3b6e
stoffee <chris@roadkill.org>
2021-09-09 12:41:35 -0700

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_network_security_group" "example" {
   name                = "acceptanceTestSecurityGroup1"
-  location            = "${azurerm_resource_group.test.name}"
+  location            = "${var.region}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
   security_rule {
